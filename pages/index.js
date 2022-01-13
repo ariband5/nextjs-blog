e4f3b33b2145ae142ab363ab19eef8fe4e2fac38
Ari Bandong <ariband@gmail.com>
2022-01-13 10:44:49 -0700

    Front-end changes
    Removed sample website

--- pages/index.js
+++ pages/index.js
@@ -22,10 +22,6 @@ export default function Home({ allPostsData}) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Hello, I'm Ari. I'm a software engineer with a background in television production. You can contact me on LinkedIn, Twitter and GitHub.</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
