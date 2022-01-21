d188f9de8c17abafd73c9b55a16dcf793204f372
Ari Bandong <ariband@gmail.com>
2022-01-21 14:39:52 -0700

    Crypto Tracker
    Added crypto tracker

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ allPostsData}) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm Ari. I'm a web developer with a background in television production. You can contact me on <Link href="https://twitter.com/guywithnosn">Twitter</Link>,<Link href="https://www.linkedin.com/in/ari-bandong/"> LinkedIn</Link> and <Link href="https://github.com/ariband5">GitHub</Link>.</p>
+        <p>Hello, I'm Ari. I'm a web developer with a background in television production. You can contact me on <Link href="https://twitter.com/guywithnosn">Twitter</Link>,<Link href="https://www.linkedin.com/in/ari-bandong/"> LinkedIn</Link> and <Link href="https://github.com/ariband5">GitHub</Link>.</p>Also, check out this<Link href="/crypto"> cryptocurrency tracker</Link> that will help you track the price of any cryptocurrency you want using the Coinbase API.
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
