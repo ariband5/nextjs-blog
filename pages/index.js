2f8805f2d0901baa68e4dc333b0f3d218b23316f
Ari Bandong <ariband@gmail.com>
2022-01-25 18:35:13 -0700

    fixed typo

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,7 @@ export default function Home({ allPostsData}) {
         <p>You can contact me on <Link href="https://twitter.com/guywithnosn">Twitter</Link> and<Link href="https://www.linkedin.com/in/ari-bandong/"> LinkedIn</Link></p>
           <p> Don't forget to check out my <Link href="https://github.com/ariband5">GitHub</Link>.</p>
         
-        <p>Also, check out this <Link href="/crypto"><a>cryptocurrency tracker</a></Link>  that uses the <Link href="https://www.api.coinbase.com/">Coinbase</Link> API.
+        <p>Also, check out this <Link href="/crypto"><a>cryptocurrency tracker</a></Link>  that uses the <Link href="https://developers.coinbase.com/api/v2">Coinbase</Link> API.
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
